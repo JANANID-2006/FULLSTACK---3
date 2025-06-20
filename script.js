f56985d242acfd4e31cfb237bed3387c1f705acb
JANANI-D2006 <jananid.23eee@kongu.edu>
2025-06-20 10:17:25 +0530

    complete eval function

---
+++ script.js
@@ -0,0 +1,29 @@
+// Show a normal message
+//console.log("Hello js");
+
+// Show an error message
+//console.error("This is an error message");
+
+// Show a warning message
+//console.warn("This is a warning message");
+
+// Show a table of items
+//console.table([
+ // { name: "Janani" },
+  //{ name: "Mouni" }
+//]);
+
+// Measure time taken by a loop
+//console.time("Loop time");
+//for (let i = 0; i < 1000000; i++) {
+  // simple loop
+//}
+//console.timeEnd("Loop time");
+
+const x=10;
+const y=20;
+const result=eval("x + y");
+console.log(result);
+"use strict";
+let a=10;
+b=20;
