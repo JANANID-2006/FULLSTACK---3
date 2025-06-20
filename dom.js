7da89173317462a11241c23121315dbe73f30f6b
JANANID-2006 <jananid.23eee@kongu.edu>
2025-06-20 14:46:10 +0530

    complete hi button

--- dom.js
+++ dom.js
@@ -1,3 +1,8 @@
-const para=document.getElementByIdById("msg");
+const para = document.getElementById("msg"); 
 console.log(para.innerText);
-para.innerText="this is set via javascript";
+para.innerText = "this is set via javascript";
+
+const hiButton = document.querySelector(".jsbutton");
+hiButton.addEventListener("click", () => {
+    console.log("Hi");
+});
